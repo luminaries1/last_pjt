cdd28716a3236099e89e5492bb45d6707f8a0cb5
luminaries <luminaries1@naver.com>
2022-11-21 10:41:01 +0900

    community design small modified

--- front-server/src/views/Community/CommunityView.vue
+++ front-server/src/views/Community/CommunityView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-		<h1>Community</h1>
-    <router-link :to="{ name: 'CreateCommunityView' }">[Create]</router-link>
-    <CommunityList />
-	</div>
+  <div class="container d-flex justify-content-center align-items-center">
+    <div class="community-box position-relative">
+      <h1 class="text-dark my-4">COMMUNITY</h1>
+      <CommunityList />
+      <router-link :to="{ name: 'CreateCommunityView' }" class="btn btn-outline-success button-border position-absolute bottom-0 end-0 m-4">Create</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,5 +44,15 @@ export default {
 </script>
 
 <style>
+.container {
+  height : 55em;
+}
 
+.community-box {
+  height: 40em;
+  width: 55em;
+  background-color: #e0e0e0;;
+  border: 2px solid #129b79;
+  border-radius: 1em ;
+}
 </style>
